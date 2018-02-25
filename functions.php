b453b2d657e11ac63c43a1882d2be8c886fb267b
Josh Herder <jdherder@gmail.com>
2018-02-25 12:52:29 -0600

    Adds support for basic og tags on posts and pages.

--- functions.php
+++ functions.php
@@ -154,3 +154,46 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Adds doctype support for opengraph
+ */
+function doctype_opengraph($output) {
+	return $output . '
+	xmlns:og="http://opengraphprotocol.org/schema/"
+	xmlns:fb="http://www.facebook.com/2008/fbml"';
+}
+add_filter('language_attributes', 'doctype_opengraph');
+
+/**
+ * Adds post and page hooks for injecting og properties into page head
+ */
+function fb_opengraph() {
+	global $post;
+
+	if(is_single() || is_page()) {
+			if(has_post_thumbnail($post->ID)) {
+					$img_src = get_the_post_thumbnail_url(get_the_ID(), 'full');
+			} else {
+					$img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
+			}
+			if($excerpt = $post->post_excerpt) {
+					$excerpt = strip_tags($post->post_excerpt);
+					$excerpt = str_replace("", "'", $excerpt);
+			} else {
+					$excerpt = get_bloginfo('description');
+			}
+			?>
+
+	<meta property="og:title" content="<?php echo the_title(); ?>"/>
+	<meta property="og:description" content="<?php echo $excerpt; ?>"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+	<meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+	<meta property="og:image" content="<?php echo $img_src; ?>"/>
+
+<?php
+	} else {
+			return;
+	}
+}
+add_action('wp_head', 'fb_opengraph', 5);
